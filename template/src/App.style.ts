7ab2bdba000daf3dfa6542720090df60263efdfe
pmezenski <pawel.mezenski@gmail.com>
2022-05-10 08:28:49 +0200

    [VRSOFT-26] Change main files

---
+++ template/src/App.style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  box-sizing: border-box;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+`;
--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { LicenseManager } from "ag-grid-enterprise";
 
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import "ag-grid-enterprise";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+import "./index.css";
+
+if (process.env.REACT_APP_AG_GRID_KEY)
+  LicenseManager.setLicenseKey(process.env.REACT_APP_AG_GRID_KEY);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
